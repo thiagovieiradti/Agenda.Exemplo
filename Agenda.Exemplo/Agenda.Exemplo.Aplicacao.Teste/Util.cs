033513a1fe55b6ad4d9a0d2657e2451a550006f3
Matheus Araújo <matheus.saraujo@gmail.com>
2017-07-23 12:37:06 -0300

    mer

---
+++ Agenda.Exemplo/Agenda.Exemplo.Aplicacao.Teste/Util.cs
@@ -0,0 +1,23 @@
+﻿using Agenda.Exemplo.Dominio.DTO;
+
+namespace Agenda.Exemplo.Aplicacao.Teste
+{
+    public static class Util
+    {
+        // extensão
+        public static GrupoDTO Clonar(this GrupoDTO dto)
+        {
+            return new GrupoDTO()
+            {
+                id = dto.id,
+                nome = dto.nome
+            };
+        }
+        
+        public static class Entidade
+        {
+            public static GrupoDTO grupo1 = new GrupoDTO() { nome = "Grupo 1" };
+            public static GrupoDTO grupo2 = new GrupoDTO() { nome = "Grupo 2" };
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Agenda.Exemplo
 Aplicação de Agenda exemplo para utilização de tecnologias nos projetos dti/supermix.
+
+
+## Banco de dados
+O MER e o script para criação do banco de dados estão na pasta \Banco.
+
+![MER](/Banco/mer.png)
+
+## Aplicação
