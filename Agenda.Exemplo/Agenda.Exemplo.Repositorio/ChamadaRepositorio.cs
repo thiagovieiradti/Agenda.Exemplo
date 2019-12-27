82a579ce71aa7ff1f744845b65f8e9103049ef63
Guilherme Oliveira <guilherme.oliveira@dtidigital.com.br>
2019-12-27 17:06:57 -0300

    Repo chamada Sql

---
+++ Agenda.Exemplo/Agenda.Exemplo.Repositorio/ChamadaRepositorio.cs
@@ -0,0 +1,33 @@
+﻿using Agenda.Exemplo.Dominio.Entidade;
+using Agenda.Exemplo.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda.Exemplo.Repositorio
+{
+    public class ChamadaRepositorio : IChamadaRepositorio
+    {
+        public int InserirChamada(Chamada chamada)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Chamada ObterChamada()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Chamada> ObterChamadas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoverChamada()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
