da39813ff7a394b519dbc4837f847e2967d52047
Guilherme Oliveira <guilherme.oliveira@dtidigital.com.br>
2019-12-27 15:39:15 -0300

    Interface chamada

---
+++ Agenda.Exemplo/Agenda.Exemplo.Dominio.Aplicacao/IChamadaAplicacao.cs
@@ -0,0 +1,17 @@
+﻿using Agenda.Exemplo.Dominio.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda.Exemplo.Dominio.Aplicacao
+{
+    public interface IChamadaAplicacao
+    {
+        int InserirChamada(ChamadaDTO chamada);
+        ContatoDTO ObterChamada();
+        IList<ContatoDTO> ObterChamadas();
+        void RemoverChamada();
+    }
+}
