0080aea187f01a9241f2ace2bc25c546f99b6a22
Guilherme Oliveira <guilherme.oliveira@dtidigital.com.br>
2019-12-27 17:28:37 -0300

    Aplicacao concluida

---
+++ Agenda.Exemplo/Agenda.Exemplo.Aplicacao/ChamadaAplicacao.cs
@@ -0,0 +1,37 @@
+﻿using Agenda.Exemplo.Aplicacao.Mapper;
+using Agenda.Exemplo.Dominio.Aplicacao;
+using Agenda.Exemplo.Dominio.DTO;
+using Agenda.Exemplo.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda.Exemplo.Aplicacao
+{
+    public class ChamadaAplicacao : IChamadaAplicacao
+    {
+        private readonly IChamadaRepositorio _chamadaRepositorio;
+
+        public int InserirChamada(ChamadaDTO chamada)
+        {
+            return _chamadaRepositorio.InserirChamada(chamada.CriarEntidade());
+        }
+
+        public ChamadaDTO ObterChamada()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ChamadaDTO> ObterChamadas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoverChamada()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
