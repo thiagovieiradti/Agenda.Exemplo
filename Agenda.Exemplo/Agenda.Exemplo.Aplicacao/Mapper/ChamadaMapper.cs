cd1b3785ef0a44313fcd997f86e959f3b40e4bed
Guilherme Oliveira <guilherme.oliveira@dtidigital.com.br>
2019-12-27 16:37:38 -0300

    Create Mapper Chamada

---
+++ Agenda.Exemplo/Agenda.Exemplo.Aplicacao/Mapper/ChamadaMapper.cs
@@ -0,0 +1,36 @@
+﻿using Agenda.Exemplo.Dominio.DTO;
+using Agenda.Exemplo.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda.Exemplo.Aplicacao.Mapper
+{
+    public static class ChamadaMapper
+    {
+        public static ChamadaDTO CriarDTO(this Chamada entidade)
+        {
+            return new ChamadaDTO()
+            {
+                chamadaId = entidade.ChamadaId,
+                contato = entidade.Contato.CriarDTO()
+            };
+        }
+
+        public static IList<ChamadaDTO> CriarDTO(this  IList<Chamada> entidade)
+        {
+            return entidade.ToList().ConvertAll(e => e.CriarDTO());
+        }
+
+        public static Chamada CriarEntidade(this ChamadaDTO dto)
+        {
+            return new Chamada()
+            {
+                ChamadaId = dto.chamadaId,
+                Contato = dto.contato.CriarEntidade()
+            };
+        }
+    }
+}
