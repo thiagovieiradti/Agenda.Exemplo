0002766150be7e923ee9a0568d4a7eb9aca5a087
Guilherme Oliveira <guilherme.oliveira@dtidigital.com.br>
2019-12-27 17:15:53 -0300

    Create entidade ChamadaMock

---
+++ Agenda.Exemplo/Agenda.Exemplo.RepositorioMock/EntidadeMock/ChamadaMock.cs
@@ -0,0 +1,25 @@
+﻿using Agenda.Exemplo.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda.Exemplo.RepositorioMock.EntidadeMock
+{
+    class ChamadaMock
+    {
+        public static Chamada chamadamock1 = new Chamada()
+        {
+            ChamadaId = 1,
+            Contato = ContatoMock.contatoMock1
+        };
+
+        public static List<Chamada> listachamadas = new List<Chamada>
+        {
+            chamadamock1
+        };
+    }
+    
+
+}
