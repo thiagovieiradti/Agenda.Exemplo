77df8d523532bc434f1eeae91d5b8cf81c544c96
Guilherme Oliveira <guilherme.oliveira@dtidigital.com.br>
2019-12-30 15:26:10 -0300

    APIChamada

---
+++ Agenda.Exemplo/Agenda.Exemplo.Site/AppScript/_API/ChamadaAPI.js
@@ -0,0 +1,20 @@
+﻿function ChamadaApi($app) {
+    this.$app = $app;
+};
+
+ChamadaAPI.prototype.ObterContatos = function (chamadaId, retorno, contexto) {
+    let url = URL_API + 'chamadas' + chamadaId;
+    if (chamadaId) url += 'chamadaId' + chamadaId;
+    this.$app.AjaxGet(url, retorno, null, contexto);
+};
+
+ChamadaAPI.prototype.InserirContato = function (chamada, retorno, contexto) {
+    let url = URL_API + 'chamadas';
+    this.$app.AjaxPost(url, chamada, retorno, null, contexto);
+};
+
+ChamadaApi.prototype.RemoverChamada = function (chamadaId, retorno, contexto) {
+    let url = URL_API + 'chamada' + chamadaId;
+    this.$app.AjaxDelete(url, retorno, null, contexto);
+};
+
